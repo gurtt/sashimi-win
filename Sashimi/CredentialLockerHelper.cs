43604e60f8edd434fab6c3783947115e1121c8e4
gurtt <6712553+gurtt@users.noreply.github.com>
2023-02-22 20:53:49 +1100

    Implement CredentialLockerHelper

---
+++ Sashimi/CredentialLockerHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Windows.Security.Credentials;
+
+public class CredentialLockerHelper
+{
+    private static readonly string resource = "Sashimi";
+    private static readonly PasswordVault vault = new();
+
+    /// <summary>
+    /// Stores the string in the credential locker under the given key.
+    /// </summary>
+    /// <param name="key">The key under which the data is stored in the credential locker.</param>
+    /// <param name="value">The values to be written to the credential locker.</param>
+    /// <exception cref="Exception">If the item isn't added successfully.</exception>
+    public static void Set(string key, string value)
+    {
+        vault.Add(new PasswordCredential(resource, key, value));
+    }
+
+    /// <summary>
+    /// Retrieves the text value from the credential locker under the given key.
+    /// </summary>
+    /// <param name="key">The key under which the data is stored in the credential locker.</param>
+    /// <exception cref="Exception">If the item isn't retrieved successfully.</exception>
+    public static string Get(string key)
+    {
+        PasswordCredential credential = vault.Retrieve(resource, key);
+        credential.RetrievePassword();
+        return credential.Password;
+    }
+
+    /// <summary>
+    /// Deletes the credential in the credential locker under the given key.
+    /// </summary>
+    /// <param name="key">The key under which the data is stored in the credential locker.</param>
+    /// <exception cref="Exception">If the item isn't deleted successfully.</exception>
+    /// 
+    public static void Remove(string key)
+    {
+        PasswordCredential credential = vault.Retrieve(resource, key);
+        vault.Remove(credential);
+    }
+
+}
