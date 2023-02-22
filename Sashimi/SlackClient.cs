8b21fecbdbe79a8e181bfa33ea9d86d050a3d55d
gurtt <6712553+gurtt@users.noreply.github.com>
2023-02-22 20:19:28 +1100

    Add set and clear status methods

--- Sashimi/SlackClient.cs
+++ Sashimi/SlackClient.cs
@@ -1,18 +1,23 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text.Json;
+using Windows.Storage.Streams;
 using Windows.Web.Http;
 
 public class SlackClient
 {
 	public struct SlackStatus
 	{
-		string status_emoji;
-		int? status_expiration;
-		string status_text;
-	}
+        #pragma warning disable IDE1006
+        public string status_emoji;
+        public int status_expiration;
+        public string status_text;
+        #pragma warning restore IDE1006
+    }
 
-	public struct SlackProfile
+    public struct SlackProfile
 	{
-		SlackStatus profile;
+        public SlackStatus profile;
 	}
 
 	private readonly string clientId;
@@ -50,6 +55,39 @@ public class SlackClient
 	{
         Uri uri = new($"https://slack.com/oauth/authorize?client_id=${clientId}&scope=${scope}");
         await Windows.System.Launcher.LaunchUriAsync(uri);
+    }
+
+    /// <summary>
+    /// Sets the user status.
+    /// </summary>
+    /// <param name="emoji">The emoji string to set the status to. A nil value uses the Slack default status emoji.</param>
+    /// <param name="text">The text to set the status to.</param>
+    public async void SetStatus(SlackStatus status)
+    {
+        try
+        {
+            Uri uri = new("https://slack.com/api/users.profile.set");
+
+            HttpStringContent request = new(JsonSerializer.Serialize(new SlackProfile {  profile = status }));
+            request.Headers["Content-type"] = "application/json; charset=utf-8";
+            request.Headers["Authorization"] = $"Bearer ${token}";
+
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(uri, request);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+        } catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+    }
+
+    /// <summary>
+    /// Clears the user status.
+    /// </summary>
+    public void ClearStatus()
+    {
+        SetStatus(new SlackStatus { status_emoji = "", status_text = "" });
     }
 
 }
